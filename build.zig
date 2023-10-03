3088af42193f642d1b05decf1f62a6ffb6fda164
Emil Lerch <emil@lerch.org>
2023-10-03 13:43:58 -0700

    allow "zig build" to work (although it will not do much)

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) !void {
         .name = "universal-lambda-zig",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/universal_lambda_build.zig" },
         .target = target,
         .optimize = optimize,
     });
